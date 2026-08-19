0c9c8057ae7f4f8454aed2a4545501051760b38c
hossamattiayacoub <hossamattia14@gmail.com>
2026-08-20 02:34:16 +0300

    Your descriptive commit message

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Paste the Google Apps Script Web App URL here after deployment.
   // Example: https://script.google.com/macros/s/AKfycb.../exec 
-  appsScriptApiUrl: 'https://script.google.com/macros/s/AKfycby_IkgHhlBd6N45zfav2VVMWIsqLFckZUcrq0CryxjUpnvrCDAZJySq4UB3fOVvjAwT_w/exec'
+  appsScriptApiUrl: 'https://script.google.com/macros/s/AKfycbySzaAs20HDC3AL1DF-c89ZCg0XSvYYCkvTI_LreS_Xi6JKVmJaO3r1hIUzuNLjiJxRwA/exec'
 };
